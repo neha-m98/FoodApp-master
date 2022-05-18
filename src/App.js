621ec9fe6cb5ecc1ee3912a69b208e45162dbe72
neha-m98 <neham1998@gmail.com>
2022-05-18 12:40:55 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import Cart from './Components/Cart/Cart';
+import Header from './Components/Layout/Header';
+import Meals from './Components/Meals/Meals';
+import { useState } from 'react';
+import CartProvider from './Store/CartProvider';
+
+function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
+  return (
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
+  );
+}
+
+export default App;
---
+++ src/Components/Meals/Meals.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import MealsSummary from './MealsSummary'
+import AvailableMeals from './AvailableMeals'
+
+const Meals = () =>{
+  return (
+    <div>
+    <MealsSummary/>
+    <AvailableMeals/>
+    </div>
+  )
+}
+
+export default Meals
