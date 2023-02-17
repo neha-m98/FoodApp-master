83ecba9e5c6299faec23d58e0fbd7e41306eef3c
neha-m98 <66993152+neha-m98@users.noreply.github.com>
2023-02-17 19:25:37 +0530

    Update MealsSummary.js
    updated spelling mistake

--- src/Components/Meals/MealsSummary.js
+++ src/Components/Meals/MealsSummary.js
@@ -5,13 +5,13 @@ const MealsSummary = () => {
     <section className={classes.summary}>
       <h2>Delicious Food delivered To You with Love!</h2>
       <p>
-      Food for thought- have you ever met anyone who really doesn’t like Italian cuisine? we haven't! 
+      Food for thought- have you ever met anyone who doesn't like Italian cuisine? We haven't! 
       Cheesy pizza, flavorful pasta, and heavenly panna cotta, just thinking about these delicacies makes me drool.
       </p>
       <p>
-      ince time immemorial Italian cuisine has enjoyed a kind of universal appeal.  Whether it's the emphasis on 
+       Since time immemorial Italian cuisine has enjoyed a universal appeal.  Whether it's the emphasis on 
       using fresh produce in Italian preparations or the blend of simple ingredients that result in one 
-      flavor-packed delight, there's no one answer to what makes Italian cuisine so popular. 
+      flavor-packed delight, there's no one answer to what makes Italian cuisine so famous.  
       </p>
     </section>
   );
